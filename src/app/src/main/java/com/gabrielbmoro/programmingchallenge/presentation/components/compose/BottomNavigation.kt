4fa8dc45ed6f75b997f237addb702ff48ec3131a
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2021-08-08 16:17:12 -0300

    Bottom bar colors modification

--- src/app/src/main/java/com/gabrielbmoro/programmingchallenge/presentation/components/compose/BottomNavigation.kt
+++ src/app/src/main/java/com/gabrielbmoro/programmingchallenge/presentation/components/compose/BottomNavigation.kt
@@ -2,13 +2,18 @@ package com.gabrielbmoro.programmingchallenge.presentation.components.compose
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -16,6 +21,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.gabrielbmoro.programmingchallenge.R
 import com.gabrielbmoro.programmingchallenge.presentation.components.compose.screens.movieList.*
+import com.gabrielbmoro.programmingchallenge.presentation.components.compose.theme.BottomBackgroundAndContentColors
 
 sealed class NavigationItem(
     @DrawableRes val icon: Int,
@@ -71,7 +77,13 @@ fun BottomNavigationBar(navController: NavController) {
         NavigationItem.FavoriteMovies
     )
 
-    BottomNavigation() {
+    val (backgroundColor, contentColor) = BottomBackgroundAndContentColors()
+
+    BottomNavigation(
+        modifier = Modifier.height(56.dp),
+        backgroundColor = backgroundColor,
+        contentColor = contentColor
+    ) {
         val navBackStackEntry by navController.currentBackStackEntryAsState()
         val currentRoute = navBackStackEntry?.destination?.route
 
