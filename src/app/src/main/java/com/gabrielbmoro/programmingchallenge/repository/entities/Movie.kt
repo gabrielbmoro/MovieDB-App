fa87e504e85e15232c8af7d4ce29e9954fbafb1c
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2022-12-09 09:26:59 -0300

    feat (improvement): Adding parcelize plugin to convert class to parcel

--- src/app/src/main/java/com/gabrielbmoro/programmingchallenge/repository/entities/Movie.kt
+++ src/app/src/main/java/com/gabrielbmoro/programmingchallenge/repository/entities/Movie.kt
@@ -1,8 +1,9 @@
 package com.gabrielbmoro.programmingchallenge.repository.entities
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Movie(
     val votesAverage: Float,
     val title: String,
@@ -12,43 +13,4 @@ data class Movie(
     var isFavorite: Boolean,
     val language: String,
     val popularity : Float
-) : Parcelable {
-
-    constructor(parcel: Parcel) : this(
-        parcel.readFloat(),
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readInt() == 1,
-        parcel.readString() ?: "",
-        parcel.readFloat()
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeFloat(votesAverage)
-        parcel.writeString(title)
-        parcel.writeString(imageUrl)
-        parcel.writeString(overview)
-        parcel.writeString(releaseDate)
-        parcel.writeInt(
-            if(isFavorite) 1 else 0
-        )
-        parcel.writeString(language)
-        parcel.writeFloat(popularity)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Movie> {
-        override fun createFromParcel(parcel: Parcel): Movie {
-            return Movie(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Movie?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
