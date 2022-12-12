bb9ea18fba7c132914e32da5db1a48770bdd0463
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2022-12-12 08:39:18 -0300

    fix (improvement): Renaming the package to match to the final location

--- src/app/src/main/java/com/gabrielbmoro/programmingchallenge/ui/screens/details/DetailsScreen.kt
+++ src/app/src/main/java/com/gabrielbmoro/programmingchallenge/ui/screens/details/DetailsScreen.kt
@@ -16,10 +16,10 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.gabrielbmoro.programmingchallenge.core.di.ConfigVariables
-import com.gabrielbmoro.programmingchallenge.ui.components.compose.Favorite
-import com.gabrielbmoro.programmingchallenge.ui.components.compose.FiveStars
-import com.gabrielbmoro.programmingchallenge.ui.components.compose.MovieDetailDescription
-import com.gabrielbmoro.programmingchallenge.ui.components.compose.MovieImage
+import com.gabrielbmoro.programmingchallenge.ui.common.widgets.Favorite
+import com.gabrielbmoro.programmingchallenge.ui.common.widgets.FiveStars
+import com.gabrielbmoro.programmingchallenge.ui.common.widgets.MovieDetailDescription
+import com.gabrielbmoro.programmingchallenge.ui.common.widgets.MovieImage
 import com.gabrielbmoro.programmingchallenge.domain.model.Movie
 import com.gabrielbmoro.programmingchallenge.ui.common.widgets.AppToolbar
 
