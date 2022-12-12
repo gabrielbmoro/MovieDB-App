e267f50c14e252fc606827cdf9f7df16c6a08859
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2022-12-11 22:51:14 -0300

    feat (settings): applying base structure in settings screen

--- src/app/src/main/java/com/gabrielbmoro/programmingchallenge/ui/screens/settings/SettingsScreen.kt
+++ src/app/src/main/java/com/gabrielbmoro/programmingchallenge/ui/screens/settings/SettingsScreen.kt
@@ -1,13 +1,27 @@
 package com.gabrielbmoro.programmingchallenge.ui.screens.settings
 
-import androidx.compose.material.Text
+import android.annotation.SuppressLint
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.gabrielbmoro.programmingchallenge.R
+import com.gabrielbmoro.programmingchallenge.ui.common.widgets.AppToolbar
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun SettingsScreen(navController: NavController, viewModel: SettingsViewModel = hiltViewModel()) {
-    Text(
-        "Settings"
+    Scaffold(
+        content = {
+
+        },
+        topBar = {
+            AppToolbar(
+                title = stringResource(id = R.string.settings),
+                backEvent = { navController.navigateUp() },
+                settingsEvent = null
+            )
+        }
     )
 }
