eae2e025ab563cdf8181ad3bca27d14004579561
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2022-12-12 10:53:23 -0300

    fix (night mode): Fixing alert look

--- src/app/src/main/java/com/gabrielbmoro/programmingchallenge/ui/common/widgets/Alert.kt
+++ src/app/src/main/java/com/gabrielbmoro/programmingchallenge/ui/common/widgets/Alert.kt
@@ -4,9 +4,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -36,6 +38,7 @@ fun Alert(
                 ) {
                     Text(
                         text = positiveText,
+                        color = Color.White
                     )
                 }
             }
