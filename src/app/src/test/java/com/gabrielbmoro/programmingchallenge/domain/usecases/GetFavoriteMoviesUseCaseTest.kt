7dc04559f282d1c2ba3e255042a62e0ba64225e8
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2022-12-26 10:46:40 -0300

    feat (unit-tests): Unit tests for Get favorite movies use case

---
+++ src/app/src/test/java/com/gabrielbmoro/programmingchallenge/domain/usecases/GetFavoriteMoviesUseCaseTest.kt
@@ -0,0 +1,57 @@
+package com.gabrielbmoro.programmingchallenge.domain.usecases
+
+import com.gabrielbmoro.programmingchallenge.domain.model.DataOrException
+import com.gabrielbmoro.programmingchallenge.domain.model.Movie
+import com.gabrielbmoro.programmingchallenge.repository.MoviesRepository
+import com.google.common.truth.Truth.assertThat
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@ExperimentalCoroutinesApi
+@RunWith(JUnit4::class)
+class GetFavoriteMoviesUseCaseTest {
+
+    lateinit var repository: MoviesRepository
+    lateinit var useCase: GetFavoriteMoviesUseCase
+
+    @Before
+    fun before() {
+        repository = mockk()
+        useCase = GetFavoriteMoviesUseCase(repository)
+    }
+
+    @Test
+    fun `should be able to get all favorite movies`() {
+        // arrange
+        val favoriteMovies = listOf(
+            Movie(
+                2f,
+                "Dragão branco",
+                "https://dragaobranco.png",
+                "Movie where Vandame shows how a good Karate fighter fights",
+                "2002-02-21",
+                language = "pt-br",
+                popularity = 2f,
+                isFavorite = false
+            )
+        )
+
+        coEvery { repository.getFavoriteMovies() }.returns(DataOrException(favoriteMovies))
+
+        runTest {
+            // act
+            val result = useCase()
+
+            // assert
+            assertThat(result.data).isEqualTo(favoriteMovies)
+            coVerify(exactly = 1) { repository.getFavoriteMovies() }
+        }
+    }
+}
