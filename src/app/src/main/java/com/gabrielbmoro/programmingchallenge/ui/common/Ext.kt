30c5fe4e4eb449163ace8e29c5f2c925c6cb7eb0
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2023-02-05 11:25:19 -0300

    feat (improvement): update the dependencies and removing unused code

---
+++ src/app/src/main/java/com/gabrielbmoro/programmingchallenge/ui/common/Ext.kt
@@ -0,0 +1,19 @@
+package com.gabrielbmoro.programmingchallenge.core
+
+import android.os.Build
+import android.os.Bundle
+import android.os.Parcelable
+import timber.log.Timber
+
+fun <T : Parcelable> Bundle.parcelableOf(key: String, type: Class<T>): T? {
+    return try {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            getParcelable(key, type)
+        } else {
+            getParcelable(key)
+        }
+    } catch (exception: Exception) {
+        Timber.e(exception)
+        null
+    }
+}
--- src/app/src/main/java/com/gabrielbmoro/programmingchallenge/ui/common/theme/Color.kt
+++ src/app/src/main/java/com/gabrielbmoro/programmingchallenge/ui/common/theme/Color.kt
@@ -1,15 +1,7 @@
 package com.gabrielbmoro.programmingchallenge.ui.common.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 val Purple500 = Color(0xff6200ee)
 val Blue700 = Color(0xff3700b3)
 val Teal200 = Color(0xff03dac5)
-
-@Composable
-fun BottomBackgroundAndContentColors(): Pair<Color,Color> {
-    return if(isSystemInDarkTheme()) Pair(Color.Black, Color.White)
-    else Pair(Color.White, Color.Black)
-}
