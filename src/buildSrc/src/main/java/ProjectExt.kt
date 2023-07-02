b5bad787600d2bc21753b9362dd943ce928af7fa
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2023-07-01 21:32:50 -0300

    fix: Trying get the authentication token from env

--- src/buildSrc/src/main/java/ProjectExt.kt
+++ src/buildSrc/src/main/java/ProjectExt.kt
@@ -1,8 +1,6 @@
 import org.gradle.api.Project
 
 fun Project.apiAuth(variableKey: String): String {
-    val localAPIToken = findProperty(variableKey).toString()
-    return localAPIToken.ifEmpty {
-        System.getenv(variableKey)
-    }
+    val localAPIToken = findProperty(variableKey)?.toString()
+    return localAPIToken ?: System.getenv(variableKey)
 }
