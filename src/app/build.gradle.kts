7d39378c7e374a1ed6ac5ed61e3dcfb593d14c58
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2023-07-02 10:50:04 -0300

    fix: Trying to avoid nullpointer exception - Bitrise build

--- src/app/build.gradle.kts
+++ src/app/build.gradle.kts
@@ -36,12 +36,12 @@ android {
 
     buildTypes {
         debug {
-            buildConfigField("String", "API_TOKEN", "\"${apiAuth("MOVIE_DB_API_TOKEN_DEBUG")}\"")
+            buildConfigField("String", "API_TOKEN", "\"${debugAPIAuth()}\"")
         }
         release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-            buildConfigField("String", "API_TOKEN", "\"${apiAuth("MOVIE_DB_API_TOKEN_RELEASE")}\"")
+            buildConfigField("String", "API_TOKEN", "\"${releaseAPIAuth()}\"")
         }
     }
     buildFeatures {
--- src/buildSrc/src/main/java/ProjectExt.kt
+++ src/buildSrc/src/main/java/ProjectExt.kt
@@ -1,6 +1,10 @@
 import org.gradle.api.Project
 
-fun Project.apiAuth(variableKey: String): String {
+private fun Project.apiAuth(variableKey: String): String {
     val localAPIToken = findProperty(variableKey)?.toString()
     return localAPIToken ?: System.getenv(variableKey)
 }
+
+fun Project.debugAPIAuth() = apiAuth("MOVIE_DB_API_TOKEN_DEBUG")
+
+fun Project.releaseAPIAuth() = apiAuth("MOVIE_DB_API_TOKEN_RELEASE")
