6bb066685059ea18f6020bed0fcedbe8b415d35a
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2023-09-02 13:10:12 -0300

    feat (feature): Testing empty state

---
+++ src/app/src/androidTest/java/com/gabrielbmoro/moviedb/ui/common/widgets/EmptyStateTest.kt
@@ -0,0 +1,38 @@
+package com.gabrielbmoro.moviedb.ui.common.widgets
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import org.junit.Rule
+import org.junit.Test
+
+class EmptyStateTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun showEmptyState() {
+        // arrange + act
+        composeTestRule.setContent {
+            EmptyState()
+        }
+
+        composeTestRule
+            .onNodeWithText("No movie here")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun showEmptyStateIcon() {
+        // arrange + act
+        composeTestRule.setContent {
+            EmptyState()
+        }
+
+        composeTestRule
+            .onNodeWithContentDescription("Sad emoji")
+            .assertExists()
+    }
+}
