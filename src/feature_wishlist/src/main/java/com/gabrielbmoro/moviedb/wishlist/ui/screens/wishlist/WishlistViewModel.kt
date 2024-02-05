528030726725ef634ad61ede3fdb2f66ef39c436
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2024-02-05 09:02:37 -0300

    Apply MVI to the Wishlist module

--- src/feature_wishlist/src/main/java/com/gabrielbmoro/moviedb/wishlist/ui/screens/wishlist/WishlistViewModel.kt
+++ src/feature_wishlist/src/main/java/com/gabrielbmoro/moviedb/wishlist/ui/screens/wishlist/WishlistViewModel.kt
@@ -1,40 +1,30 @@
 package com.gabrielbmoro.moviedb.wishlist.ui.screens.wishlist
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.gabrielbmoro.moviedb.core.ui.mvi.ViewModelMVI
 import com.gabrielbmoro.moviedb.wishlist.domain.usecases.GetFavoriteMoviesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.async
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class WishlistViewModel @Inject constructor(
     private val getFavoriteMoviesUseCase: GetFavoriteMoviesUseCase
-) : ViewModel() {
-
-    private val _uiState = MutableStateFlow(
-        WishlistUIState(
-            favoriteMovies = null,
-            isLoading = false
-        )
-    )
-    val uiState = _uiState.stateIn(
-        viewModelScope,
-        SharingStarted.Eagerly,
-        _uiState.value
-    )
+) : ViewModelMVI<Any, WishlistUIState>() {
 
     fun load() = viewModelScope.async {
         getFavoriteMoviesUseCase().collect { movies ->
-            _uiState.update {
-                it.copy(
+            updateState(
+                getState().copy(
                     favoriteMovies = movies
                 )
-            }
+            )
         }
     }
+
+    override suspend fun execute(intent: Any): WishlistUIState {
+        TODO("Not yet implemented")
+    }
+
+    override fun defaultEmptyState(): WishlistUIState = WishlistUIState()
 }
