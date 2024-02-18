c374545536577bed230f86dfad0cf360e1ffb533
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2024-02-18 09:41:44 -0300

    Delete button

---
+++ src/core/src/main/java/com/gabrielbmoro/moviedb/core/ui/widgets/DeleteButton.kt
@@ -0,0 +1,58 @@
+package com.gabrielbmoro.moviedb.core.ui.widgets
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.gabrielbmoro.moviedb.core.R
+
+private val deleteCardContainerColor = Color(0xFFFFDAD4)
+private val deleteCardTextColor = Color(0xFF851300)
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DeleteButton(onClick: () -> Unit, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier,
+        onClick = onClick,
+        colors = CardDefaults.cardColors(
+            containerColor = deleteCardContainerColor
+        ),
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.Delete,
+                tint = Color.Red,
+                contentDescription = null,
+            )
+
+            Text(
+                style = MaterialTheme.typography.titleSmall,
+                color = deleteCardTextColor,
+                text = stringResource(id = R.string.delete),
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DeleteButtonPreview() {
+    DeleteButton(onClick = {})
+}
--- src/core/src/main/java/com/gabrielbmoro/moviedb/core/ui/widgets/MovieCard.kt
+++ src/core/src/main/java/com/gabrielbmoro/moviedb/core/ui/widgets/MovieCard.kt
@@ -4,7 +4,6 @@ import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.AnimationVector1D
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.DraggableState
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.draggable
@@ -18,15 +17,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.Text
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
@@ -36,6 +33,7 @@ import kotlinx.coroutines.launch
 
 const val threshold = -120f
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MovieCard(
     imageUrl: String?,
@@ -50,21 +48,22 @@ fun MovieCard(
 
     Box(modifier = modifier) {
 
-        Button(modifier = Modifier.align(Alignment.CenterEnd), onClick = {}) {
-            Text("Delete")
-        }
+        DeleteButton(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.align(Alignment.CenterEnd)
+        )
 
         Card(
             modifier = Modifier
                 .fillMaxWidth()
-                .clip(RoundedCornerShape(12.dp))
                 .height(dimensionResource(id = R.dimen.card_view_image_height))
                 .draggable(
                     state = draggableState,
                     orientation = Orientation.Horizontal
                 )
-                .offset(x = offsetX.targetValue.dp)
-                .clickable(onClick = onClick)
+                .offset(x = offsetX.targetValue.dp),
+            shape = RoundedCornerShape(12.dp),
+            onClick = onClick
         ) {
             Row {
                 MovieImage(
