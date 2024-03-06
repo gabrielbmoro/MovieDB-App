20b01615fafa849113c92547407628b624278f49
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2024-03-06 09:00:37 -0300

    Remove truth library

--- src/domain/src/main/java/com/gabrielbmoro/moviedb/domain/entities/VideoStream.kt
+++ src/domain/src/main/java/com/gabrielbmoro/moviedb/domain/entities/VideoStream.kt
@@ -12,37 +12,4 @@ data class VideoStream(
     val size: Int,
     val official: Boolean,
     val type: String
-) : Parcelable {
-
-    companion object {
-        fun mockVideoStreamNotTrailer() = VideoStream(
-            id = "12341",
-            name = "Chuck Norris - the battle",
-            key = "lViasfV1",
-            site = "YouTube",
-            official = true,
-            type = "Not trailer",
-            size = 1024
-        )
-
-        fun mockVideoStreamTrailer() = VideoStream(
-            id = "12341",
-            name = "Chuck Norris - the battle",
-            key = "lViasfV1",
-            site = "YouTube",
-            official = true,
-            type = "Trailer",
-            size = 1024
-        )
-
-        fun mockVideoStreamNotYouTube() = VideoStream(
-            id = "12341",
-            name = "Chuck Norris - the battle",
-            key = "lViasfV1",
-            site = "Vimeo",
-            official = true,
-            type = "Trailer",
-            size = 1024
-        )
-    }
-}
+) : Parcelable
