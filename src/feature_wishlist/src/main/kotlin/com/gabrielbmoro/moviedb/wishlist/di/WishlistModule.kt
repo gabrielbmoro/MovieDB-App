ef853244281935889408b6c695a89451da9ea302
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2024-03-17 11:23:35 -0300

    Implement to the wishlist screen - ScreenModel

--- src/feature_wishlist/src/main/kotlin/com/gabrielbmoro/moviedb/wishlist/di/WishlistModule.kt
+++ src/feature_wishlist/src/main/kotlin/com/gabrielbmoro/moviedb/wishlist/di/WishlistModule.kt
@@ -1,11 +1,10 @@
 package com.gabrielbmoro.moviedb.wishlist.di
 
 import com.gabrielbmoro.moviedb.wishlist.ui.screens.wishlist.WishlistViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val featureWishlistModule = module {
-    viewModel {
+    factory {
         WishlistViewModel(
             favoriteMovieUseCase = get(),
             getFavoriteMoviesUseCase = get(),
--- src/feature_wishlist/src/main/kotlin/com/gabrielbmoro/moviedb/wishlist/ui/screens/wishlist/WishlistScreen.kt
+++ src/feature_wishlist/src/main/kotlin/com/gabrielbmoro/moviedb/wishlist/ui/screens/wishlist/WishlistScreen.kt
@@ -12,73 +12,77 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.koin.getScreenModel
 import com.gabrielbmoro.moviedb.core.ui.widgets.BubbleLoader
 import com.gabrielbmoro.moviedb.core.ui.widgets.EmptyState
 import com.gabrielbmoro.moviedb.core.ui.widgets.ScreenScaffold
 import com.gabrielbmoro.moviedb.feature.wishlist.R
 import com.gabrielbmoro.moviedb.domain.entities.Movie
 import com.gabrielbmoro.moviedb.wishlist.ui.widgets.MovieList
-import org.koin.androidx.compose.koinViewModel
 
-@Composable
-fun WishlistScreen(
-    navigateToDetailsScreen: ((Movie) -> Unit),
-    bottomBar: @Composable (() -> Unit),
-    viewModel: WishlistViewModel = koinViewModel()
-) {
-    val uiState = viewModel.uiState.collectAsStateWithLifecycle()
-    val snackbarHostState = remember { SnackbarHostState() }
-    val lazyListState = rememberLazyListState()
+class WishlistScreen(
+    private val navigateToDetailsScreen: (Movie) -> Unit,
+    private val bottomBar: @Composable () -> Unit,
+) : Screen {
+    @Composable
+    override fun Content() {
+        val viewModel = getScreenModel<WishlistViewModel>()
+        val uiState = viewModel.uiState.collectAsStateWithLifecycle()
+        val snackbarHostState = remember { SnackbarHostState() }
+        val lazyListState = rememberLazyListState()
 
-    ScreenScaffold(
-        showTopBar = true,
-        appBarTitle = stringResource(id = R.string.wishlist),
-        bottomBar = bottomBar,
-        snackBarHost = {
-            SnackbarHost(snackbarHostState)
-        }
-    ) {
-        when {
-            uiState.value.isLoading -> {
-                BubbleLoader(
-                    color = MaterialTheme.colorScheme.primary,
-                    modifier = Modifier.align(Alignment.Center)
-                )
+        ScreenScaffold(
+            showTopBar = true,
+            appBarTitle = stringResource(id = R.string.wishlist),
+            bottomBar = bottomBar,
+            snackBarHost = {
+                SnackbarHost(snackbarHostState)
             }
-
-            uiState.value.favoriteMovies?.isEmpty() == true -> {
-                EmptyState(
-                    modifier = Modifier.align(Alignment.Center)
-                )
-            }
-
-            else -> {
-                uiState.value.favoriteMovies?.let { moviesList ->
-                    MovieList(
-                        moviesList = moviesList,
-                        onSelectMovie = navigateToDetailsScreen,
-                        lazyListState = lazyListState,
-                        onDeleteMovie = { movie ->
-                            viewModel.accept(WishlistUserIntent.DeleteMovie(movie))
-                        },
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .align(Alignment.TopCenter)
+        ) {
+            when {
+                uiState.value.isLoading -> {
+                    BubbleLoader(
+                        color = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier.align(Alignment.Center)
                     )
+                }
+
+                uiState.value.favoriteMovies?.isEmpty() == true -> {
+                    EmptyState(
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                }
+
+                else -> {
+                    uiState.value.favoriteMovies?.let { moviesList ->
+                        MovieList(
+                            moviesList = moviesList,
+                            onSelectMovie = navigateToDetailsScreen,
+                            lazyListState = lazyListState,
+                            onDeleteMovie = { movie ->
+                                viewModel.accept(WishlistUserIntent.DeleteMovie(movie))
+                            },
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .align(Alignment.TopCenter)
+                        )
+                    }
                 }
             }
         }
+
+        LaunchedEffect(
+            key1 = uiState.value,
+            block = {
+                uiState.value.resultMessage?.let { resultMessage ->
+                    snackbarHostState.showSnackbar(resultMessage)
+                    viewModel.accept(WishlistUserIntent.ResultMessageReset)
+                }
+
+                viewModel.accept(WishlistUserIntent.LoadMovies)
+            }
+        )
     }
 
-    LaunchedEffect(
-        key1 = uiState.value,
-        block = {
-            uiState.value.resultMessage?.let { resultMessage ->
-                snackbarHostState.showSnackbar(resultMessage)
-                viewModel.accept(WishlistUserIntent.ResultMessageReset)
-            }
-
-            viewModel.accept(WishlistUserIntent.LoadMovies)
-        }
-    )
 }
