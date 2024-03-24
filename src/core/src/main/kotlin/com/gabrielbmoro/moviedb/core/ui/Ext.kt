3dc08d663c411c021924ba65012125e25958bd02
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2024-03-24 18:23:33 -0300

    Remove unnecessary code

--- src/core/src/main/kotlin/com/gabrielbmoro/moviedb/core/ui/Ext.kt
+++ src/core/src/main/kotlin/com/gabrielbmoro/moviedb/core/ui/Ext.kt
@@ -1,24 +1,7 @@
 package com.gabrielbmoro.moviedb.core.ui
 
 import android.app.Activity
-import android.os.Build
-import android.os.Bundle
-import android.os.Parcelable
 import com.google.android.material.elevation.SurfaceColors
-import timber.log.Timber
-
-fun <T : Parcelable> Bundle.parcelableOf(key: String, type: Class<T>): T? {
-    return try {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            getParcelable(key, type)
-        } else {
-            getParcelable(key)
-        }
-    } catch (exception: Exception) {
-        Timber.e(exception)
-        null
-    }
-}
 
 fun Activity.syncTopBarsColors() {
     val color = SurfaceColors.SURFACE_2.getColor(this)
