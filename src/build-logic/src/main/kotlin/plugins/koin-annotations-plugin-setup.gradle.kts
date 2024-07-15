9af65d8c05bf5c4d4e4fea9d6caffa0b9bcab631
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2024-07-14 22:45:26 -0300

    Koin annotations plugin

---
+++ src/build-logic/src/main/kotlin/plugins/koin-annotations-plugin-setup.gradle.kts
@@ -0,0 +1,47 @@
+import ext.koinAnnotationsDependency
+import ext.koinCompiler
+import ext.koinCoreDependency
+import ext.libs
+import org.jetbrains.kotlin.gradle.dsl.KotlinCompile
+
+plugins {
+    id("com.google.devtools.ksp")
+    id("kotlin-multiplatform")
+}
+
+kotlin {
+    sourceSets {
+        val commonMain by getting {
+            kotlin.srcDir("build/generated/ksp/metadata/commonMain/kotlin")
+
+            dependencies {
+                api(libs.koinCoreDependency())
+                api(libs.koinAnnotationsDependency())
+            }
+        }
+    }
+}
+
+dependencies {
+    add("kspCommonMainMetadata", libs.koinCompiler())
+}
+
+// WORKAROUND: ADD this dependsOn("kspCommonMainKotlinMetadata") instead of above dependencies
+tasks.withType<KotlinCompile<*>>().configureEach {
+    if (name != "kspCommonMainKotlinMetadata") {
+        dependsOn("kspCommonMainKotlinMetadata")
+    }
+}
+
+afterEvaluate {
+    tasks.filter {
+        it.name.contains("SourcesJar", true)
+    }.forEach {
+        println("SourceJarTask====>${it.name}")
+        it.dependsOn("kspCommonMainKotlinMetadata")
+    }
+}
+
+ksp {
+    arg("KOIN_CONFIG_CHECK", "false")
+}
