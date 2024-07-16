197113df0c651b2c83bb54a85a617654bfcbb2a0
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2024-07-16 07:51:35 -0300

    Detekt files

---
+++ src/build-logic/src/main/kotlin/plugins/detekt-plugin-setup.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("io.gitlab.arturbosch.detekt")
+}
+
+detekt {
+    config.setFrom(file(project.rootDir.path.plus("/config/detekt/detekt.yml")))
+    buildUponDefaultConfig = true
+
+    source.from(
+        "src/commonMain/kotlin",
+        "src/commonTest/kotlin"
+    )
+
+}
