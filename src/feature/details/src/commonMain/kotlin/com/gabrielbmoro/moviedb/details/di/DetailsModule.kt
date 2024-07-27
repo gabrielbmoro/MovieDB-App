3358271b24b949ffe102c7cd72e0a8f6a130f95f
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2024-07-27 12:20:59 -0300

    Backing to dsl - details viewmodel

--- src/feature/details/src/commonMain/kotlin/com/gabrielbmoro/moviedb/details/di/DetailsModule.kt
+++ src/feature/details/src/commonMain/kotlin/com/gabrielbmoro/moviedb/details/di/DetailsModule.kt
@@ -1,34 +1,18 @@
 package com.gabrielbmoro.moviedb.details.di
 
 import com.gabrielbmoro.moviedb.details.ui.screens.details.DetailsViewModel
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import org.koin.compose.viewmodel.dsl.viewModel
-import org.koin.core.annotation.ComponentScan
-import org.koin.core.annotation.Factory
-import org.koin.core.annotation.Module
 import org.koin.dsl.module
-import org.koin.ksp.generated.module
 
-val featureDetailsModule = DetailsModule().module.apply {
-    includes(
-        module {
-            viewModel<DetailsViewModel> {
-                DetailsViewModel(
-                    getMovieDetailsUseCase = get(),
-                    favoriteMovieUseCase = get(),
-                    isFavoriteMovieUseCase = get(),
-                    ioDispatcher = Dispatchers.IO
-                )
-            }
-        }
-    )
-}
-
-@Module
-@ComponentScan("com.gabrielbmoro.moviedb.details.ui")
-class DetailsModule {
-    @Factory
-    fun ioDispatcher(): CoroutineDispatcher = Dispatchers.IO
+val featureDetailsModule = module {
+    viewModel<DetailsViewModel> {
+        DetailsViewModel(
+            getMovieDetailsUseCase = get(),
+            favoriteMovieUseCase = get(),
+            isFavoriteMovieUseCase = get(),
+            ioDispatcher = Dispatchers.IO
+        )
+    }
 }
