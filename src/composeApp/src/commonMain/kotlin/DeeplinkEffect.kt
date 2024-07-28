62413aaaf7772316d8535fa68bb69c883ee067c3
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2024-07-28 13:41:31 -0300

    Solve some issue

--- src/composeApp/src/commonMain/kotlin/DeeplinkEffect.kt
+++ src/composeApp/src/commonMain/kotlin/DeeplinkEffect.kt
@@ -1,18 +1,31 @@
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.navigation.NavHostController
 import com.gabrielbmoro.moviedb.platform.navigation.SEARCH_QUERY_ARGUMENT_KEY
 import com.gabrielbmoro.moviedb.platform.navigation.Screen
 import com.gabrielbmoro.moviedb.platform.navigation.detailsRoute
 import com.gabrielbmoro.moviedb.platform.navigation.searchRoute
 import dev.theolm.rinku.compose.ext.DeepLinkListener
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 @Composable
 fun DeeplinkEffect(
-    deepLinkBarrier: MutableStateFlow<String?>
+    navigator: NavHostController
 ) {
+    val deepLinkNavigation = remember { DeepLinkNavigation() }
     val coroutineContext = rememberCoroutineScope()
+
+    LaunchedEffect(Unit) {
+        deepLinkNavigation.barrier.collectLatest { route ->
+            route?.let {
+                navigator.navigate(route)
+            }
+        }
+    }
 
     DeepLinkListener {
         when (it.pathSegments.firstOrNull()) {
@@ -20,7 +33,7 @@ fun DeeplinkEffect(
                 it.pathSegments.getOrNull(1)?.split("-")?.firstOrNull()?.toLongOrNull()
                     ?.let { movieId ->
                         coroutineContext.launch {
-                            deepLinkBarrier.emit(
+                            deepLinkNavigation.barrier.emit(
                                 Screen.Details.route.detailsRoute(movieId)
                             )
                         }
@@ -29,14 +42,14 @@ fun DeeplinkEffect(
 
             Screen.Wishlist.firstSegment -> {
                 coroutineContext.launch {
-                    deepLinkBarrier.emit(Screen.Wishlist.route)
+                    deepLinkNavigation.barrier.emit(Screen.Wishlist.route)
                 }
             }
 
             Screen.Search.firstSegment -> {
                 it.parameters[SEARCH_QUERY_ARGUMENT_KEY]?.let { query ->
                     coroutineContext.launch {
-                        deepLinkBarrier.emit(
+                        deepLinkNavigation.barrier.emit(
                             Screen.Search.route.searchRoute(query)
                         )
                     }
@@ -48,4 +61,8 @@ fun DeeplinkEffect(
             }
         }
     }
+}
+
+class DeepLinkNavigation {
+    val barrier = MutableStateFlow<String?>(null)
 }
--- src/composeApp/src/commonMain/kotlin/RootApp.kt
+++ src/composeApp/src/commonMain/kotlin/RootApp.kt
@@ -1,6 +1,4 @@
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.gabrielbmoro.moviedb.details.ui.screens.details.DetailsScreen
@@ -12,14 +10,10 @@ import com.gabrielbmoro.moviedb.platform.navigation.addSearchScreen
 import com.gabrielbmoro.moviedb.platform.navigation.addWishlistScreen
 import com.gabrielbmoro.moviedb.search.ui.screens.search.SearchScreen
 import com.gabrielbmoro.moviedb.wishlist.ui.screens.wishlist.WishlistScreen
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.collectLatest
 
 @Composable
 fun RootApp() {
     val navigator = rememberNavController()
-
-    val deepLinkNavigation = remember { DeepLinkNavigation() }
 
     NavHost(
         startDestination = Screen.Movies.route,
@@ -27,15 +21,6 @@ fun RootApp() {
     ) {
         addMoviesScreen {
             MoviesScreen(navigator = navigator)
-
-            // Defined deeplink barrier to the first screen destination
-            LaunchedEffect(Unit) {
-                deepLinkNavigation.barrier.collectLatest { route ->
-                    route?.let {
-                        navigator.navigate(it)
-                    }
-                }
-            }
         }
 
         addMovieDetailsScreen { movieId ->
@@ -53,10 +38,6 @@ fun RootApp() {
     }
 
     DeeplinkEffect(
-        deepLinkBarrier = deepLinkNavigation.barrier,
+        navigator = navigator,
     )
-}
-
-class DeepLinkNavigation {
-    val barrier = MutableStateFlow<String?>(null)
 }
