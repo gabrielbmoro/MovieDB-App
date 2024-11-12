c91ce02c4a5322ab72112bc5e51e4b37c668f93e
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2024-11-11 21:04:40 -0300

    Remove unnecessary import

--- src/build-logic/src/main/kotlin/plugins/util-setup-plugin.gradle.kts
+++ src/build-logic/src/main/kotlin/plugins/util-setup-plugin.gradle.kts
@@ -2,7 +2,6 @@ import com.github.codandotv.popcorn.domain.input.PopcornConfiguration
 import com.github.codandotv.popcorn.domain.input.PopcornProject
 import com.github.codandotv.popcorn.domain.input.ProjectType
 import com.github.codandotv.popcorn.domain.rules.NoDependencyRule
-import gradle.kotlin.dsl.accessors._57fa814fe7c4b1263b4498fcf3d078af.popcornGuineapigConfig
 
 plugins {
     id("kmp-library-plugin")
