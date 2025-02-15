308b4bc4d4ebfd0e556b5f8f0bd583f74f060d00
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2025-02-15 18:53:00 -0300

    Implement youtube player on iOS

--- src/util/media/src/iosMain/kotlin/com/gabrielbmoro/moviedb/media/VideoPlayer.ios.kt
+++ src/util/media/src/iosMain/kotlin/com/gabrielbmoro/moviedb/media/VideoPlayer.ios.kt
@@ -1,11 +1,39 @@
 package com.gabrielbmoro.moviedb.media
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.UIKitView
+import platform.WebKit.WKWebView
+import platform.WebKit.WKWebViewConfiguration
+import platform.WebKit.WKWebsiteDataStore
 
 @Composable
 actual fun VideoPlayer(
     videoId: String,
-    shouldStartMuted: Boolean,
     modifier: Modifier
-) = Unit
+) {
+    val webView = remember {
+        val embedHTML = videoId.videoIdToEmbedHTML()
+        val configuration = WKWebViewConfiguration()
+        configuration.websiteDataStore = WKWebsiteDataStore.defaultDataStore()
+        configuration.limitsNavigationsToAppBoundDomains = false
+
+        val webView = WKWebView()
+        webView.configuration.websiteDataStore = WKWebsiteDataStore.defaultDataStore()
+        webView.configuration.limitsNavigationsToAppBoundDomains = false
+        webView.loadHTMLString(
+            string = embedHTML,
+            baseURL = null
+        )
+        webView
+    }
+
+    UIKitView(
+        modifier = modifier,
+        factory = {
+            webView
+        },
+        update = { }
+    )
+}
