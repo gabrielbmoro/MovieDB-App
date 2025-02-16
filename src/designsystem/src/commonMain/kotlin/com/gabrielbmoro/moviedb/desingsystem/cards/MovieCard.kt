4541b7436a3590b9e4f3befb38986a0a7850fee5
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2025-02-16 15:39:30 -0300

    Keep the size dynamically and avoid cut the description

--- src/designsystem/src/commonMain/kotlin/com/gabrielbmoro/moviedb/desingsystem/cards/MovieCard.kt
+++ src/designsystem/src/commonMain/kotlin/com/gabrielbmoro/moviedb/desingsystem/cards/MovieCard.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
