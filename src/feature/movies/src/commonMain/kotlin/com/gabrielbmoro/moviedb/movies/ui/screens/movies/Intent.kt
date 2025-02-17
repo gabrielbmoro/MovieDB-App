067d61abbcc9f8dfd1825d1c0afeaa3daa452d4d
PierreVieira <pierrevieiraggg@gmail.com>
2025-02-17 10:58:51 -0300

    Refactor: Change Movies Intent to sealed interface
    The `Intent` class in the movies module has been refactored from a `sealed class` to a `sealed interface`. The `RequestMoreMovies`, `Setup`, and `SelectFilterMenuItem` intents are now defined as part of this interface.

--- src/feature/movies/src/commonMain/kotlin/com/gabrielbmoro/moviedb/movies/ui/screens/movies/Intent.kt
+++ src/feature/movies/src/commonMain/kotlin/com/gabrielbmoro/moviedb/movies/ui/screens/movies/Intent.kt
@@ -2,10 +2,10 @@ package com.gabrielbmoro.moviedb.movies.ui.screens.movies
 
 import com.gabrielbmoro.moviedb.movies.domain.model.FilterMenuItem
 
-sealed class Intent {
-    data object RequestMoreMovies : Intent()
+sealed interface Intent {
+    data object RequestMoreMovies : Intent
 
-    data object Setup : Intent()
+    data object Setup : Intent
 
-    data class SelectFilterMenuItem(val menuItem: FilterMenuItem) : Intent()
+    data class SelectFilterMenuItem(val menuItem: FilterMenuItem) : Intent
 }
