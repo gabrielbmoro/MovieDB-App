199ead020f7555834bd2a25e030534182ed975a9
PierreVieira <pierrevieiraggg@gmail.com>
2025-02-18 02:16:30 -0300

    Remove detekt plugin from the project. Update the project's dependencies versions file.

--- src/build.gradle.kts
+++ src/build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
     alias(libs.plugins.jetbrains.compose) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.buildkonfig.plugin) apply false
-    alias(libs.plugins.detekt.plugin)
 }
 
 private val detektConfiguration by configurations.creating
