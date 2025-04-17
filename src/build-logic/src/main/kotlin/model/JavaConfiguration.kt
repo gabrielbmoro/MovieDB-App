47c2adcaf6553c9338a07d5bbea1ce80c6ba56b5
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2025-04-17 18:12:05 -0300

    Remove deprecated code to define jvm target version

--- src/build-logic/src/main/kotlin/model/JavaConfiguration.kt
+++ src/build-logic/src/main/kotlin/model/JavaConfiguration.kt
@@ -1,8 +1,10 @@
 package model
 
 import org.gradle.api.JavaVersion
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 internal data class JavaConfiguration(
     val version: JavaVersion,
     val javaVmTarget: String,
+    val jvmTarget: JvmTarget,
 )
