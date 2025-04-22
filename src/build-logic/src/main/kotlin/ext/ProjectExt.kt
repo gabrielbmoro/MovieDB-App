530c0d6031d1953672b7613ea6c42071a123cab2
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2025-04-22 10:47:39 -0300

    Replace kover plugin in favor of kover aggregated plugin

--- src/build-logic/src/main/kotlin/ext/ProjectExt.kt
+++ src/build-logic/src/main/kotlin/ext/ProjectExt.kt
@@ -1,32 +1,9 @@
 package ext
 
-import config.ConfigurationKeys
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalog
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.getByType
-
-private fun Project.isEligibleForCoverage(): Boolean {
-    return ConfigurationKeys.ELIGIBLE_MODULES_FOR_COVERAGE.contains(name)
-}
-
-internal fun Project.configureKover(kover: (Project) -> Unit) {
-    rootProject.subprojects.forEach { module ->
-        when {
-            module.subprojects.isEmpty() -> {
-                if (module.isEligibleForCoverage()) {
-                    kover(module)
-                }
-            }
-
-            else -> {
-                module.subprojects.filter { subModule ->
-                    subModule.isEligibleForCoverage()
-                }.forEach(kover)
-            }
-        }
-    }
-}
 
 internal val Project.libs: VersionCatalog
     get() {
