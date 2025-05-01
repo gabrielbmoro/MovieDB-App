039d2a249c4b7e9fe0d28681c1e866ee0b39cc0e
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2025-05-01 10:05:18 -0300

    JVM Updates + Kover Aggregated Plugin (#337)
    * Remove deprecated code to define jvm target version
    
    * Replace kover plugin in favor of kover aggregated plugin
    
    * Replace kover plugin in favor of kover aggregated plugin

--- src/build.gradle.kts
+++ src/build.gradle.kts
@@ -2,7 +2,6 @@
 
 plugins {
     alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.kover) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.jetbrains.compose) apply false
     alias(libs.plugins.compose.compiler) apply false
