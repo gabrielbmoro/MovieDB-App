760f30c68c7f2ea468dc7ff6846577f14b0070f7
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2026-04-26 12:18:32 -0300

    Mutable domain entity: Movie.isFavorite should be immutable (#404)
    * Avoid mutable domain entity
    
    * Update kotlin_gradle_plugin to use version reference
    
    - Update `kotlin_gradle_plugin` in `libs.versions.toml` to reference the `kotlin` version instead of the hardcoded value `2.2.20`.

--- src/domain/src/commonMain/kotlin/com/gabrielbmoro/moviedb/domain/entities/Movie.kt
+++ src/domain/src/commonMain/kotlin/com/gabrielbmoro/moviedb/domain/entities/Movie.kt
@@ -8,7 +8,7 @@ data class Movie(
     val backdropImageUrl: String?,
     val overview: String,
     val releaseDate: String,
-    var isFavorite: Boolean,
+    val isFavorite: Boolean,
     val language: String,
     val popularity: Float,
 ) {
@@ -32,12 +32,12 @@ data class Movie(
                 id = 12L,
                 isFavorite = true,
                 overview = "It is a long established fact that a reader will be distracted by the " +
-                        "readable content of a page when looking at its layout. The point of using " +
-                        "Lorem Ipsum is that it has a more-or-less normal distribution of letters, " +
-                        "as opposed to using 'Content here, content here', making it look like " +
-                        "readable English. Many desktop publishing packages and web page editors " +
-                        "now use Lorem Ipsum as their default model text, and a search for 'lorem " +
-                        "ipsum' will uncover many web sites still in their infancy. Various v",
+                    "readable content of a page when looking at its layout. The point of using " +
+                    "Lorem Ipsum is that it has a more-or-less normal distribution of letters, " +
+                    "as opposed to using 'Content here, content here', making it look like " +
+                    "readable English. Many desktop publishing packages and web page editors " +
+                    "now use Lorem Ipsum as their default model text, and a search for 'lorem " +
+                    "ipsum' will uncover many web sites still in their infancy. Various v",
                 title = "Why do we use it?",
                 releaseDate = "02-03-2020",
                 posterImageUrl = "https://image/bucket/s3/ga2.png",
