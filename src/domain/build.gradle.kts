1db9b6711320c6ff8c5763f6e854dc6e8d558e3f
gabrielmoro <gabrielbronzattimoro.es@gmail.com>
2026-05-13 18:24:29 -0300

    Remove arc violation test

--- src/domain/build.gradle.kts
+++ src/domain/build.gradle.kts
@@ -5,9 +5,6 @@ plugins {
 
 kotlin {
     sourceSets {
-        commonMain.dependencies {
-            implementation(projects.util.logging)
-        }
         commonTest.dependencies {
             implementation(libs.bundles.test.multiplatform)
         }
